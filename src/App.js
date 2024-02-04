ad4c49f6d7da33e09c1706e04f9d5a01bd37930e
Serhii Vasyliev <vasiliev.hub@gmail.com>
2024-02-03 22:06:30 -0500

    Adding product filtering by category

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Items from "./components/Items";
+import Categories from "./components/Categories";
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       orders: [],
+      currentItems: [],
       items: [
         {
           id: 1,
           title: 'Chair grey',
           img: 'chair-grey.jpeg',
           desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-          category: 'chair',
+          category: 'chairs',
           price: '49.99'
         },
         {
@@ -51,18 +53,32 @@ class App extends React.Component {
         }
       ]
     }
+    this.state.currentItems = this.state.items
     this.addToOrder = this.addToOrder.bind(this)
     this.deleteOrder = this.deleteOrder.bind(this)
+    this.chooseCategory = this.chooseCategory.bind(this)
   }
   render() {
     return (
       <div className="wrapper">
         <Header orders={this.state.orders} onDelete={this.deleteOrder} />
-        <Items items={this.state.items} onAdd={this.addToOrder} />
+        <Categories chooseCategory={this.chooseCategory} />
+        <Items items={this.state.currentItems} onAdd={this.addToOrder} />
         <Footer />
       </div>
     )
   }
+
+  chooseCategory(category) {
+    if (category === 'all') {
+      this.setState({ currentItems: this.state.items })
+      return
+    }
+    this.setState({
+      currentItems: this.state.items.filter(el => el.category === category)
+    })
+  }
+
   deleteOrder(id) {
     this.setState({ orders: this.state.orders.filter(el => el.id !== id) })
   }
